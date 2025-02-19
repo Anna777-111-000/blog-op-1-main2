c24af9861a5c365e33fb47e1cf37cfb2c71ea08a
Anna777-111-000 <anneremneva@yandex.ru>
2025-02-19 22:21:20 +0300

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from .models import Category, Post
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
     categories = Category.objects.all()
@@ -35,4 +35,22 @@ def post(request, slug):
 
 
 # TODO: Доделать функцию вывода
-# def get_category(request, slug):
+def get_category(request, slug):
+      category = get_object_or_404(Category, slug=slug)  
+    posts = Post.objects.filter(category=category, status='published') #
+    paginator = Paginator(posts, 5)
+    page_number = request.GET.get('page')  
+
+    try:
+        posts_page = paginator.get_page(page_number) 
+    except PageNotAnInteger:
+        posts_page = paginator.page(1) 
+    except EmptyPage:
+        posts_page = paginator.page(paginator.num_pages) 
+    context = {
+        'category': category,
+        'posts': posts_page,
+    }
+
+    return render(request, 'blog/category_posts.html', context)
+
